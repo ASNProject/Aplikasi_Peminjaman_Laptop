86b5d0868a960673407fb538543c2cf6bec18a47
Arief Setyo Nugroho <49858542+ASNProject@users.noreply.github.com>
2023-02-01 19:44:49 +0700

    update: return loan crud api

--- return_loan_screen.py
+++ return_loan_screen.py
@@ -2,8 +2,12 @@ import tkinter
 from tkinter import *
 from PIL import ImageTk, Image
 import customtkinter
+import sqlite3
+import datetime as dt
 
 root = Tk()
+database = "database/apl_database.db"
+connection = sqlite3.connect(database)
 
 # ################### SETUP SCREEN ################ #
 root.title('Aplikasi Peminjaman Laptop - Halaman Pengembalian')
@@ -40,7 +44,9 @@ t3 = Label(root, text='Jl. Raya Solo - Yogyakarta, Mereden, Sendangtirto, Kec. K
 t3.place(relx=0.08, y=70)
 
 # ##### BODY ##### #
-b1 = customtkinter.CTkButton(master=root, corner_radius=10, text="SCAN ID CARD", height=40, width=200)
+b1 = customtkinter.CTkButton(master=root, corner_radius=10, text="SCAN ID CARD", height=40, width=200,
+                             command=lambda: scanidcard(234)
+                             )
 b1.place(relx=0.08, rely=0.2)
 
 t4 = Label(root, text='HASIL SCAN:', font=("Arial bold", 12))
@@ -50,46 +56,136 @@ t5 = Label(root, text='ID', font=("Arial", 12))
 t5.place(relx=0.08, rely=0.32)
 t6 = Label(root, text=':', font=("Arial", 12))
 t6.place(relx=0.13, rely=0.32)
+ids = tkinter.StringVar()
+t7 = Label(root, textvariable=ids, font=("Arial", 12))
+t7.place(relx=0.14, rely=0.32)
 
-t7 = Label(root, text='NAMA', font=("Arial", 12))
-t7.place(relx=0.08, rely=0.35)
-t8 = Label(root, text=':', font=("Arial", 12))
-t8.place(relx=0.13, rely=0.35)
+t8 = Label(root, text='NAMA', font=("Arial", 12))
+t8.place(relx=0.08, rely=0.35)
+t9 = Label(root, text=':', font=("Arial", 12))
+t9.place(relx=0.13, rely=0.35)
+nama = tkinter.StringVar()
+t10 = Label(root, textvariable=nama, font=("Arial", 12))
+t10.place(relx=0.14, rely=0.35)
 
-t9 = Label(root, text='MEMBER', font=("Arial", 12))
-t9.place(relx=0.08, rely=0.38)
-t10 = Label(root, text=':', font=("Arial", 12))
-t10.place(relx=0.13, rely=0.38)
+t11 = Label(root, text='MEMBER', font=("Arial", 12))
+t11.place(relx=0.08, rely=0.38)
+t12 = Label(root, text=':', font=("Arial", 12))
+t12.place(relx=0.13, rely=0.38)
+member = tkinter.StringVar()
+t13 = Label(root, textvariable=member, font=("Arial", 12))
+t13.place(relx=0.14, rely=0.38)
 
-b2 = customtkinter.CTkButton(master=root, corner_radius=10, text="SCAN ID LAPTOP", height=40, width=200)
+b2 = customtkinter.CTkButton(master=root, corner_radius=10, text="SCAN ID LAPTOP", height=40, width=200,
+                             command=lambda: scanidlaptop(123))
 b2.place(relx=0.75, rely=0.2)
 
-t4 = Label(root, text='HASIL SCAN:', font=("Arial bold", 12))
-t4.place(relx=0.75, rely=0.29)
+t14 = Label(root, text='HASIL SCAN:', font=("Arial bold", 12))
+t14.place(relx=0.75, rely=0.29)
 
-t5 = Label(root, text='ID', font=("Arial", 12))
-t5.place(relx=0.75, rely=0.32)
-t6 = Label(root, text=':', font=("Arial", 12))
-t6.place(relx=0.80, rely=0.32)
+t15 = Label(root, text='ID', font=("Arial", 12))
+t15.place(relx=0.75, rely=0.32)
+t16 = Label(root, text=':', font=("Arial", 12))
+t16.place(relx=0.80, rely=0.32)
+idlaptop = tkinter.StringVar()
+t17 = Label(root, textvariable=idlaptop, font=("Arial", 12))
+t17.place(relx=0.81, rely=0.32)
 
-t7 = Label(root, text='NAMA', font=("Arial", 12))
-t7.place(relx=0.75, rely=0.35)
-t8 = Label(root, text=':', font=("Arial", 12))
-t8.place(relx=0.80, rely=0.35)
+t18 = Label(root, text='BRAND', font=("Arial", 12))
+t18.place(relx=0.75, rely=0.35)
+t19 = Label(root, text=':', font=("Arial", 12))
+t19.place(relx=0.80, rely=0.35)
+brand = tkinter.StringVar()
+t20 = Label(root, textvariable=brand, font=("Arial", 12))
+t20.place(relx=0.81, rely=0.35)
 
-t9 = Label(root, text='UNIT', font=("Arial", 12))
-t9.place(relx=0.75, rely=0.38)
-t10 = Label(root, text=':', font=("Arial", 12))
-t10.place(relx=0.80, rely=0.38)
+t21 = Label(root, text='UNIT', font=("Arial", 12))
+t21.place(relx=0.75, rely=0.38)
+t22 = Label(root, text=':', font=("Arial", 12))
+t22.place(relx=0.80, rely=0.38)
+unit = tkinter.StringVar()
+t23 = Label(root, textvariable=unit, font=("Arial", 12))
+t23.place(relx=0.81, rely=0.38)
 
-t11 = Label(root, text='NOTA PENGEMBALIAN', font=("Arial bold", 14))
-t11.place(relx=0.05, rely=0.46)
+t24 = Label(root, text='NOTA PENGEMBALIAN', font=("Arial bold", 14))
+t24.place(relx=0.05, rely=0.46)
 
 c = Canvas(root, bg="white",
            height=300, width=800)
 c.place(relx=0.05, rely=0.5)
 
-b3 = customtkinter.CTkButton(master=root, corner_radius=10, text="PENGEMBALIAN", height=40, width=270)
+t50 = Label(root, text='APLIKASI PEMINJAMAN LAPTOP', fg="black", bg="white", font=("Arial bold", 16))
+t50.place(relx=0.08, rely=0.55)
+t51 = Label(root, text='AKADEMI ANGKATAN UDARA YOGYAKARTA', fg="black", bg="white", font=("Arial bold", 14))
+t51.place(relx=0.08, rely=0.58)
+
+t25 = Label(root, fg="black", bg="white", text='ID', font=("Arial", 12))
+t25.place(relx=0.08, rely=0.64)
+t26 = Label(root, fg="black", bg="white", text=':', font=("Arial", 12))
+t26.place(relx=0.18, rely=0.64)
+np_id = tkinter.StringVar()
+t27 = Label(root, fg="black", bg="white", textvariable=np_id, font=("Arial", 12))
+t27.place(relx=0.19, rely=0.64)
+
+t28 = Label(root, fg="black", bg="white", text='NAMA', font=("Arial", 12))
+t28.place(relx=0.08, rely=0.67)
+t29 = Label(root, fg="black", bg="white", text=':', font=("Arial", 12))
+t29.place(relx=0.18, rely=0.67)
+np_nama = tkinter.StringVar()
+t30 = Label(root, fg="black", bg="white", textvariable=np_nama, font=("Arial", 12))
+t30.place(relx=0.19, rely=0.67)
+
+t31 = Label(root, fg="black", bg="white", text='MEMBER', font=("Arial", 12))
+t31.place(relx=0.08, rely=0.70)
+t32 = Label(root, fg="black", bg="white", text=':', font=("Arial", 12))
+t32.place(relx=0.18, rely=0.70)
+np_member = tkinter.StringVar()
+t33 = Label(root, fg="black", bg="white", textvariable=np_member, font=("Arial", 12))
+t33.place(relx=0.19, rely=0.70)
+
+t34 = Label(root, fg="black", bg="white", text='LAPTOP ID', font=("Arial", 12))
+t34.place(relx=0.08, rely=0.73)
+t35 = Label(root, fg="black", bg="white", text=':', font=("Arial", 12))
+t35.place(relx=0.18, rely=0.73)
+np_laptopid = tkinter.StringVar()
+t36 = Label(root, fg="black", bg="white", textvariable=np_laptopid, font=("Arial", 12))
+t36.place(relx=0.19, rely=0.73)
+
+t37 = Label(root, fg="black", bg="white", text='BRAND', font=("Arial", 12))
+t37.place(relx=0.08, rely=0.76)
+t38 = Label(root, fg="black", bg="white", text=':', font=("Arial", 12))
+t38.place(relx=0.18, rely=0.76)
+np_brand = tkinter.StringVar()
+t39 = Label(root, fg="black", bg="white", textvariable=np_brand, font=("Arial", 12))
+t39.place(relx=0.19, rely=0.76)
+
+t40 = Label(root, fg="black", bg="white", text='UNIT', font=("Arial", 12))
+t40.place(relx=0.08, rely=0.79)
+t41 = Label(root, fg="black", bg="white", text=':', font=("Arial", 12))
+t41.place(relx=0.18, rely=0.79)
+np_unit = tkinter.StringVar()
+t42 = Label(root, fg="black", bg="white", textvariable=np_unit, font=("Arial", 12))
+t42.place(relx=0.19, rely=0.79)
+
+t43 = Label(root, fg="black", bg="white", text='JATUH TEMPO', font=("Arial", 12))
+t43.place(relx=0.08, rely=0.82)
+t44 = Label(root, fg="black", bg="white", text=':', font=("Arial", 12))
+t44.place(relx=0.18, rely=0.82)
+np_tanggalpinjam = tkinter.StringVar()
+t45 = Label(root, fg="black", bg="white", textvariable=np_tanggalpinjam, font=("Arial", 12))
+t45.place(relx=0.19, rely=0.82)
+
+t46 = Label(root, fg="black", bg="white", text='TANGGAL KEMBALI', font=("Arial", 12))
+t46.place(relx=0.08, rely=0.85)
+t47 = Label(root, fg="black", bg="white", text=':', font=("Arial", 12))
+t47.place(relx=0.18, rely=0.85)
+np_tanggalkembali = tkinter.StringVar()
+t48 = Label(root, fg="black", bg="white", textvariable=np_tanggalkembali, font=("Arial", 12))
+t48.place(relx=0.19, rely=0.85)
+
+b3 = customtkinter.CTkButton(master=root, corner_radius=10, text="PENGEMBALIAN", height=40, width=270,
+                             command=lambda: upload_data()
+                             )
 b3.place(relx=0.73, rely=0.52)
 b4 = customtkinter.CTkButton(master=root, corner_radius=10, text="PRINT", height=40, width=270)
 b4.place(relx=0.73, rely=0.6)
@@ -97,7 +193,195 @@ b4 = customtkinter.CTkButton(master=root, corner_radius=10, text="KEMBALI", heig
                              command=root.destroy)
 b4.place(relx=0.73, rely=0.85)
 
-t12 = Label(root, text='Copyright: Aplikasi Peminjaman Laptop 2023', font=("Arial", 12))
-t12.place(relx=0.8, rely=0.96)
+t49 = Label(root, text='Copyright: Aplikasi Peminjaman Laptop 2023', font=("Arial", 12))
+t49.place(relx=0.8, rely=0.96)
+
+# #### def #### #
+idmember = tkinter.StringVar()
+statuslaptop = tkinter.StringVar()
+
+
+def scanidcard(rfid):
+    try:
+        val = int(rfid)
+        try:
+            data = (val,)
+            q = "SELECT * FROM member_data WHERE rfid= ?"
+            cursor = connection.execute(q, data)
+            data_row = cursor.fetchmany(rfid)
+            for row in data_row:
+                ids.set(row[0])
+                idmember.set(row[1])
+                nama.set(row[2])
+                member.set(row[3])
+                statuslaptop.set(row[5])
+        except sqlite3.Error as error:
+            print("error:", error)
+    except:
+        print("ada error")
+
+
+def scanidlaptop(rfid):
+    check_member = t13.getvar(t13.cget("textvariable"))
+    laptop = tkinter.StringVar()
+    status = tkinter.StringVar()
+
+    if check_member == "AKTIF":
+        try:
+            val = int(rfid)
+            try:
+                data = (val,)
+                q = "SELECT * FROM laptop_data WHERE rfid= ?"
+                cursor = connection.execute(q, data)
+                data_row = cursor.fetchmany(rfid)
+                for row in data_row:
+                    idlaptop.set(row[0])
+                    laptop.set(row[1])
+                    brand.set(row[2])
+                    unit.set(row[3])
+                    status.set(row[4])
+
+                    if laptop.get() == idmember.get():
+                        if status.get() == statuslaptop.get():
+                            print("Data Sesuai")
+                            np_id.set(t7.getvar(t7.cget("textvariable")))
+                            np_nama.set(t10.getvar(t10.cget("textvariable")))
+                            np_member.set(t13.getvar(t13.cget("textvariable")))
+                            np_laptopid.set(t17.getvar(t17.cget("textvariable")))
+                            np_brand.set(t20.getvar(t20.cget("textvariable")))
+                            np_unit.set(t23.getvar(t23.cget("textvariable")))
+
+                        else:
+                            laptop_is_loan()
+                    else:
+                        cant_loan()
+            except sqlite3.Error as error:
+                print("error:", error)
+            try:
+                datas = (val,)
+                print(datas)
+                qs = "SELECT * FROM peminjaman_data WHERE laptop_id= ?"
+                cursors = connection.execute(qs, datas)
+                data_rows = cursors.fetchmany(rfid)
+                for rows in data_rows:
+                    date = dt.datetime.now()
+                    format_date = f"{date:%d %b %Y}"
+                    np_tanggalkembali.set(format_date)
+                    np_tanggalpinjam.set(rows[7])
+                    print(rows[7])
+            except sqlite3.Error as error:
+                print("error:", error)
+        except:
+            print("ada error")
+    else:
+        not_member()
+
+
+def upload_data():
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM peminjaman_data WHERE rfid_member= ?", (int(t7.getvar(t7.cget("textvariable"))),))
+    cursor.execute("UPDATE member_data SET status=? WHERE rfid=?",
+                   ('NOT LOAN', int(t7.getvar(t7.cget("textvariable")))))
+    cursor.execute("UPDATE laptop_data SET status=? WHERE rfid=?",
+                   ('NOT LOAN', int(t17.getvar(t17.cget("textvariable")))))
+    connection.commit()
+    done_upload()
+    # ids.set("")
+    # nama.set("")
+    # member.set("")
+    # idlaptop.set("")
+    # brand.set("")
+    # unit.set("")
+    # np_id.set("")
+    # np_nama.set("")
+    # np_member.set("")
+    # np_laptopid.set("")
+    # np_brand.set("")
+    # np_unit.set("")
+    # np_tanggalpinjam.set("")
+    # np_tanggalkembali.set("")
+    print("terkirim")
+
+
+# #### POP UP #### #
+def not_member():
+    top = Toplevel(root)
+
+    top_width = 320
+    top_height = 120
+    # get screen dimension
+    top_screen_width = top.winfo_screenwidth()
+    top_screen_height = top.winfo_screenheight()
+    # find the center point
+    top_center_x = int(top_screen_width / 2 - top_width / 2)
+    top_center_y = int(top_screen_height / 2 - top_height / 2)
+    # set the position of the window to the center of the screen
+    top.geometry(f'{top_width}x{top_height}+{top_center_x}+{top_center_y}')
+    top.resizable(False, False)
+    top.title("Pemberitahuan!")
+    t26 = Label(top, text='Maaf anda bukan member!\nSilahkan daftar/aktivasi terlebih dahulu!',
+                font=("Arial bold", 14))
+    t26.place(relx=.5, rely=.5, anchor=CENTER)
+
+
+def laptop_is_loan():
+    top = Toplevel(root)
+
+    top_width = 320
+    top_height = 120
+    # get screen dimension
+    top_screen_width = top.winfo_screenwidth()
+    top_screen_height = top.winfo_screenheight()
+    # find the center point
+    top_center_x = int(top_screen_width / 2 - top_width / 2)
+    top_center_y = int(top_screen_height / 2 - top_height / 2)
+    # set the position of the window to the center of the screen
+    top.geometry(f'{top_width}x{top_height}+{top_center_x}+{top_center_y}')
+    top.resizable(False, False)
+    top.title("Pemberitahuan!")
+    t26 = Label(top, text='Maaf laptop sudah dipinjam!\nMungkin terjadi kesalahan peminjaman!',
+                font=("Arial bold", 14))
+    t26.place(relx=.5, rely=.5, anchor=CENTER)
+
+
+def cant_loan():
+    top = Toplevel(root)
+
+    top_width = 320
+    top_height = 120
+    # get screen dimension
+    top_screen_width = top.winfo_screenwidth()
+    top_screen_height = top.winfo_screenheight()
+    # find the center point
+    top_center_x = int(top_screen_width / 2 - top_width / 2)
+    top_center_y = int(top_screen_height / 2 - top_height / 2)
+    # set the position of the window to the center of the screen
+    top.geometry(f'{top_width}x{top_height}+{top_center_x}+{top_center_y}')
+    top.resizable(False, False)
+    top.title("Pemberitahuan!")
+    t26 = Label(top, text='Maaf verifikasi tidak sesuai!\nSilahkan periksa id member dan id laptop!',
+                font=("Arial bold", 14))
+    t26.place(relx=.5, rely=.5, anchor=CENTER)
+
+
+def done_upload():
+    top = Toplevel(root)
+
+    top_width = 320
+    top_height = 120
+    # get screen dimension
+    top_screen_width = top.winfo_screenwidth()
+    top_screen_height = top.winfo_screenheight()
+    # find the center point
+    top_center_x = int(top_screen_width / 2 - top_width / 2)
+    top_center_y = int(top_screen_height / 2 - top_height / 2)
+    # set the position of the window to the center of the screen
+    top.geometry(f'{top_width}x{top_height}+{top_center_x}+{top_center_y}')
+    top.resizable(False, False)
+    top.title("Pemberitahuan!")
+    t26 = Label(top, text='Data telah tersimpan!\nTerimakasih!',
+                font=("Arial bold", 14))
+    t26.place(relx=.5, rely=.5, anchor=CENTER)
+
 
 root.mainloop()
